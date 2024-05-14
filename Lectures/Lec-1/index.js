3f31908744f7717e568a62e4511ff19af12b91a8
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2024-05-15 00:24:59 +0530

    GSAP CDN added, basic animation created

--- Lectures/Lec-1/index.js
+++ Lectures/Lec-1/index.js
@@ -1,1 +1,17 @@
-console.log("hello");
+//move from initial to final position
+gsap.to("#box1",{
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"#DC143C"
+})
+
+//move from final to final initial
+gsap.from("#box2",{
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"#DC143C"
+})
